26f474a3723f3b53612fee8106bf6e1fb334dfd5
ismaelsadeeq <abubakarsadiqismail@proton.me>
2025-12-23 16:10:04 +0000

    Deploying to gh-pages from @ ismaelsadeeq/my-website@b74bc96ef72b066962485b662f16ec36b3e1b735 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/cv/";
           },
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/books/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
@@ -375,11 +382,11 @@ ninja.data = [{
             window.location.href = "/al-folio/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-the-beginning-of-infinity",
+          title: 'The Beginning of Infinity',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/al-folio/books/the_godfather/";
+              window.location.href = "/al-folio/books/the_beginning_of_infinity/";
             },},{
         id: 'social-cv',
         title: 'CV',
@@ -407,20 +414,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/al-folio/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
