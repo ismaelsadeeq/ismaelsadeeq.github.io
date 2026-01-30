9992603d792941362ccc5daf35e9cca6adc643b1
ismaelsadeeq <abubakarsadiqismail@proton.me>
2026-01-30 16:02:30 +0000

    Deploying to gh-pages from @ ismaelsadeeq/ismaelsadeeq.github.io@258fc2b58650ad59493c0ab08083320450ae2ba9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
-        },{id: "post-determining-blocktemplate-fee-increase-using-fee-rate-diagram",
+        },{id: "post-managing-multiple-projects-with-git-worktrees",
+        
+          title: "Managing Multiple Projects with Git Worktrees",
+        
+        description: "A guide to using Git worktrees for managing multiple branches and tasks simultaneously.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/managing-multiple-projects-with-git-worktrees/";
+          
+        },
+      },{id: "post-determining-blocktemplate-fee-increase-using-fee-rate-diagram",
         
           title: "Determining BlockTemplate Fee Increase Using Fee Rate Diagram",
         
